ce015f06ff91b3d62659959f9f9af5b1bb04a8b1
Pedro França <jppfeitosa@gmail.com>
2023-02-10 21:34:41 -0300

    hello updated

--- lab1/src/Hello_Dirlididi.java
+++ lab1/src/Hello_Dirlididi.java
@@ -3,8 +3,11 @@
 public class Hello_Dirlididi {
 	
 	public static void main(String[] args) {
+		Integer x = 0;
+		Integer y = 5;
 		
-		System.out.println("Hello Dirlididi! Acho que � s� isso por enquanto! =]");		
+		System.out.println("Hello Dirlididi! Acho que � s� isso por enquanto! =]");	
+		System.out.println(x+y);	
 		
 	}
 }
