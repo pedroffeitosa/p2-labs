e23475af28287e46634de12e70c43fa0f11ab305
João Pedro <jppfeitosa@gmail.com>
2023-05-16 20:21:56 -0300

    Disciplina class updated

--- lab2/src/Disciplina.java
+++ lab2/src/Disciplina.java
@@ -1,44 +1,42 @@
-
-
 public class Disciplina {
-	public String nomeDisciplina;
-	public double valorNota = 0, nota1=0, nota2=0, nota3=0, nota4=0;
-	public int tempoHoras=0, nota=1;
-	public boolean aprovado = false;
-	public double media = 0;
-	
-	public Disciplina(String nomeDisciplina) {
-		this.nomeDisciplina = nomeDisciplina;
-	}
+    private String nomeDisciplina;
+    private double[] notas = new double[4];
+    private int tempoHoras;
+    
+    public Disciplina(String nomeDisciplina) {
+        this.nomeDisciplina = nomeDisciplina;
+    }
 
-	public void cadastraHoras(int tempoHoras) {
-		this.tempoHoras += tempoHoras;
-	}
+    public void cadastraHoras(int tempoHoras) {
+        this.tempoHoras += tempoHoras;
+    }
 
-	public void cadastraNota(int nota, double valorNota) {
-		if (nota == 1) {
-			this.nota1 = valorNota;
-		}
-		if (nota == 2) {
-			this.nota2 = valorNota;
-		}
-		if (nota == 3) {
-			this.nota3 = valorNota;
-		}
-		if (nota == 4) {
-			this.nota4 = valorNota;
-		}
-	}
-	
-	public boolean aprovado() {
-		media = (nota1 + nota2 + nota3 + nota4)/4;
-		if (media >= 7) {
-			aprovado = true;
-		}
-		return aprovado;
-	}
-	
-	public String toString() {
-		return nomeDisciplina + " " + tempoHoras + " " + media + " " + "[" + nota1 + ", " + nota2 + ", " + nota3 + ", " + nota4 + "]";
-	}
+    public void cadastraNota(int nota, double valorNota) {
+        if (nota >= 1 && nota <= 4) {
+            notas[nota - 1] = valorNota;
+        } else {
+            System.out.println("Nota inválida. As notas válidas vão de 1 a 4.");
+        }
+    }
+
+    public boolean aprovado() {
+        double somaNotas = 0;
+        for (double nota : notas) {
+            somaNotas += nota;
+        }
+        double media = somaNotas / notas.length;
+        return media >= 7;
+    }
+
+    public String toString() {
+        return nomeDisciplina + " " + tempoHoras + " " + calculaMedia() + " " + Arrays.toString(notas);
+    }
+
+    private double calculaMedia() {
+        double somaNotas = 0;
+        for (double nota : notas) {
+            somaNotas += nota;
+        }
+        return somaNotas / notas.length;
+    }
 }
